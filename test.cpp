b3a0c2e262beee6e8e661552ba179eed1f7040a2
Demtsov <108861332+Demtsov@users.noreply.github.com>
2023-12-20 20:08:28 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-// Функция, которую будем тестировать
+
 int divide(int a, int b) {
     if (b == 0) {
         throw "Division by zero";
@@ -8,18 +8,17 @@ int divide(int a, int b) {
     return a / b;
 }
 
-// Простая функция для демонстрации покрытия кода
+
 int add(int a, int b) {
     return a + b;
 }
 
 int main() {
-    // Примеры использования функций
+   
     std::cout << "Result of division: " << divide(10, 2) << std::endl;
     std::cout << "Result of addition: " << add(5, 7) << std::endl;
 
     try {
-        // Вызов функции с делением на ноль для демонстрации исключения
         std::cout << "Result of division: " << divide(8, 0) << std::endl;
     } catch (const char* msg) {
         std::cerr << "Exception caught: " << msg << std::endl;
